6bdeb58e05ac6aa68ee113444e007003cac2ad2a
uttam rawat <uttamrawat146>
2022-02-12 23:46:54 -0800

    new commit on branch

---
+++ index.js
@@ -0,0 +1,31 @@
+const http = require('http');
+const fs = require('fs');
+var requests = require('requests');
+const homefile = fs.readFileSync("home.html","utf-8");
+
+const replaceval = (tempVal, orgVal) =>{
+    let temperature = tempVal.replace("{%tempval%}",orgVal.main.temp);
+    temperature = temperature.replace("{%tempmin%}",orgVal.main.temp_min);
+    temperature = temperature.replace("{%tempmax%}",orgVal.main.temp_max);
+    temperature = temperature.replace("{%location%}",orgVal.name);
+    temperature = temperature.replace("{%country%}",orgVal.sys.country);
+    temperature = temperature.replace("{%tempstatus%}",orgVal.weather[0].main);
+    return temperature;
+}
+const server = http.createServer((req,res) =>{
+    if(req.url == "/"){
+        requests('https://api.openweathermap.org/data/2.5/weather?q=delhi&appid=e0046c1ac9c25c1ed02ec713a593008d')
+        .on('data', (chunk) => {
+          const objdata = JSON.parse(chunk);
+          const arrData = [objdata];
+          const realTimeData = arrData.map((val) => replaceval(homefile, val)).join("");
+          res.write(realTimeData);
+          //console.log(arrData);
+        })
+        .on('end', (err) => {
+          if (err) return console.log('connection closed due to errors', err);
+          res.end();
+        });
+    }
+})
+server.listen(3000,'127.0.0.1');
